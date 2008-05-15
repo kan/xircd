20b89f48d648733f264263b36303f0f0b12babe1
kan <kan@d0d07461-0603-4401-acd4-de1884942a52>
2008-05-15 18:20:11 +0000

    I made a mistake in the usage of Sub::Exporter.
    git-svn-id: http://svn.coderepos.org/share/lang/perl/XIRCD/trunk@11670 d0d07461-0603-4401-acd4-de1884942a52

--- lib/XIRCD/Component.pm
+++ lib/XIRCD/Component.pm
@@ -1,41 +1,24 @@
 package XIRCD::Component;
 use Moose;
 
-use Sub::Exporter;
-use Sub::Name qw/subname/;
-
-my %exports = (
-    debug => sub {
-        return subname 'XIRCD::Component::debug' => sub (@) {
-            print @_, "\n\n" if $ENV{XIRCD_DEBUG};
-        };
-    },
-    get_args => sub {
-        return subname 'XIRCD::Component::get_args' => sub (@) {
-            return @_[9..19];
-        };
-    },
-    http_alias => sub {
-        return subname 'XIRCD::Component::http_alias' => sub {
-            my $self = shift;
-            return 'twitter_' . $self->get_session_id;
-        };
-    },
-);
-
-my $exporter = Sub::Exporter::build_exporter(
-    {
-        exports => \%exports,
-        groups  => { default => [':all'] }
+use Sub::Exporter -setup => {
+    exports => [qw(debug get_args http_alias)],
+    groups  => { 
+        default => [ -all ], 
     }
-);
+};
 
-sub import {
-    my ( $pkg, $subclass ) = @_;
+sub debug (@) {
+    print @_, "\n\n" if $ENV{XIRCD_DEBUG};
+}
 
-    return if caller() eq 'main';
+sub get_args (@) {
+    return @_[9..19];
+}
 
-    goto $exporter;
+sub http_alias {
+    my $self = shift;
+    return 'twitter_' . $self->get_session_id;
 }
 
 
