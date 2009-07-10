9a638311ccd3cdba3115c0927c96a929afb61b45
Tokuhiro Matsuno <tokuhirom@gmail.com>
2009-07-10 21:24:20 +0900

    recent mouse++

--- lib/XIRCD/Component.pm
+++ lib/XIRCD/Component.pm
@@ -6,18 +6,6 @@ use Coro::Specific;
 
 our @EXPORT = qw(self debug get_args yield post publish_message publish_notice timer);
 
-# XXX this is silly. mouse does not have enough feature!
-sub init_class {
-    my $klass = shift;
-    my $meta  = any_moose('::Meta::Class')->initialize($klass);
-    $meta->superclasses( any_moose('::Object') )
-      unless $meta->superclasses;
-
-    no strict 'refs';
-    no warnings 'redefine';
-    *{ $klass . '::meta' } = sub { $meta };
-}
-
 sub import {
     strict->import;
     warnings->import;
@@ -26,12 +14,7 @@ sub import {
     my $mode = shift;
     my $pkg = caller(0);
     unless ($mode && $mode eq '-nocomponent') {
-        if (Any::Moose::moose_is_preferred) {
-            Moose->import({ into_level => 1 });
-        } else {
-            init_class($pkg);
-            Mouse->export_to_level(1);
-        }
+        any_moose()->import({into_level => 1});
         XIRCD::Base->_setup(scalar caller(0));
         XIRCD::Base->export_to_level(1);
         any_moose('Util')->can('apply_all_roles')->(
